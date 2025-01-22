f2a0d8f4630605e5caf79a3b6fcf5dbabbacd291
Petr Pavlín <pe.pavlin@gmail.com>
2025-01-22 16:00:05 +0100

    fix runworker script

--- server/run_worker.sh
+++ server/run_worker.sh
@@ -1,3 +1,12 @@
-export QUEUE_NAME=parse_files
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
+QUEUE_NAME=parse_files
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $SCRIPT_DIR
+
+cd ..
+
+source venv/bin/activate
+
 rq worker --with-scheduler $QUEUE_NAME

93b0fb78a8f334700684a785366cdc16e3db481d
Petr Pavlín <pe.pavlin@gmail.com>
2025-12-08 22:44:46 +0100

    Remove deprecated images, debug scripts, and visualizer for paper detection module

--- paper_detection/__init__.py
+++ paper_detection/__init__.py
@@ -2,10 +2,8 @@
 Paper Detection Module
 
 Isolated module for paper detection in images.
-Detects paper using OpenCV and visualizes it with a blue frame.
 """
 
 from .detector import PaperDetector
-from .visualizer import PaperVisualizer
 
-__all__ = ['PaperDetector', 'PaperVisualizer']
+__all__ = ['PaperDetector']

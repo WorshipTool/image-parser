d7520619f213705d2f6ff582dd7c237709705c5b
Petr Pavlín <pe.pavlin@gmail.com>
2025-12-29 17:55:32 +0100

    refactor: Remove outdated test files and add new tests for orient_by_text functionality

--- paper_transform/document_orient/__init__.py
+++ paper_transform/document_orient/__init__.py
@@ -5,6 +5,6 @@ Text-based orientation detection for scanned documents using OCR.
 Part of the paper_transform module.
 """
 
-from .orient_by_text import orient_by_text, batch_orient_text
+from .orient_by_text import orient_by_text
 
-__all__ = ['orient_by_text', 'batch_orient_text']
+__all__ = ['orient_by_text']
